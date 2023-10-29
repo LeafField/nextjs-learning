29895dc2b8a2d952635afe207890cdd554b91d03
LeafField <109466326+LeafField@users.noreply.github.com>
2023-10-30 00:13:07 +0900

    セクション8の途中まで完了

---
+++ mycv/src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entiry';
+import { Report } from './reports/report.entiry';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    UsersModule,
+    ReportsModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [User, Report],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
